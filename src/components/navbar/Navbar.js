d90471fda4e86ea22b03b5e5497d500aa5bb1488
jakazzy <jakazzy@gmail.com>
2019-11-01 20:10:52 +0000

    add navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react'
-import HomeIcon from '../icons/home/HomeIcon'
-import BoardIcon from '../icons/board/BoardIcon'
+import React, { Component } from "react";
+import HomeIcon from "../icons/home/HomeIcon";
+import BoardIcon from "../icons/board/BoardIcon";
+import Input from "../input/Input";
+import "./Navbar.css";
 
 export class Navbar extends Component {
-  render () {
+  render() {
     return (
-      <div>
+      <div className="navbar">
         <HomeIcon />
         <BoardIcon />
+        <Input />
       </div>
-    )
+    );
   }
 }
 
-export default Navbar
+export default Navbar;
