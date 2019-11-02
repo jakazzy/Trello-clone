31f9de1db2be9ffbb0cbeaa3cfdefd28a9712fd5
jakazzy <jakazzy@gmail.com>
2019-11-02 17:11:51 +0000

    add Ui of card column

--- src/containers/CardColumn/CardColumn.js
+++ src/containers/CardColumn/CardColumn.js
@@ -1,13 +1,130 @@
 import React from "react";
-import Navbar from "../../components/navbar/Navbar";
-import "./board.css";
+import "./CardColumn.css";
 
-function Board() {
+function CardColumn() {
   return (
-    <div className="task-board">
-      <Navbar />
+    <div className="sub-board">
+      <div className="column">
+        <div className="list-header">
+          <h2 className="list-header-text">To do</h2>
+          <div className="list-header-icon">...</div>
+        </div>
+        <div className="column-row-cards">
+          <ul className="row-cards">
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+          </ul>
+        </div>
+        <div className="card-bottom">
+          <span className="card-bottom-icon">+</span>
+          <span className="card-bottom-text">Add another card</span>
+        </div>
+      </div>
+
+      <div className="column">
+        <div className="list-header">
+          <h2 className="list-header-text">To do</h2>
+          <div className="list-header-icon">...</div>
+        </div>
+        <div className="column-row-cards">
+          <ul className="row-cards">
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+          </ul>
+        </div>
+        <div className="card-bottom">
+          <span className="card-bottom-icon">+</span>
+          <span className="card-bottom-text">Add another card</span>
+        </div>
+      </div>
+
+      <div className="column">
+        <div className="list-header">
+          <h2 className="list-header-text">To do</h2>
+          <div className="list-header-icon">...</div>
+        </div>
+        <div className="column-row-cards">
+          <ul className="row-cards">
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+          </ul>
+        </div>
+        <div className="card-bottom">
+          <span className="card-bottom-icon">+</span>
+          <span className="card-bottom-text">Add another card</span>
+        </div>
+      </div>
+
+      <div className="column">
+        <div className="list-header">
+          <h2 className="list-header-text">To do</h2>
+          <div className="list-header-icon">...</div>
+        </div>
+        <div className="column-row-cards">
+          <ul className="row-cards">
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+            <li className="card">
+              <span className="card-info">Game of life(react)</span>
+            </li>
+          </ul>
+        </div>
+        <div className="card-bottom">
+          <span className="card-bottom-icon">+</span>
+          <span className="card-bottom-text">Add another card</span>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Board;
+export default CardColumn;
