4f7f10d54db9ce1f1a41790d8f3d04a3fc0fd739
jakazzy <jakazzy@gmail.com>
2019-11-02 19:27:17 +0000

    data to be displayed in state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import CardColumn from "./containers/CardColumn/CardColumn";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
+  const { board } = useState([
+    {
+      title: "Plan",
+      data: [
+        { card: "bake cake", id: 6 },
+        { card: "wash utensils", id: 7 },
+        { card: "Read book", id: 8 },
+        { card: "complete proofread", id: 9 }
+      ],
+      id: 1
+    },
+    {
+      title: "to do",
+      data: [
+        { card: "water plants", id: 10 },
+        { card: "iron dresses", id: 11 },
+        { card: "Help friends", id: 12 },
+        { card: "braid hair", id: 13 }
+      ],
+      id: 2
+    },
+    {
+      title: "doing",
+      data: [
+        { card: "comb hair", id: 14 },
+        { card: "buy prepaid", id: 15 },
+        { card: "sweep room", id: 16 },
+        { card: "fetch water", id: 17 }
+      ],
+      id: 3
+    },
+    {
+      title: "Done",
+      data: [
+        { card: "watch soccer", id: 18 },
+        { card: "repair meter", id: 19 },
+        { card: "clean room", id: 20 },
+        { card: "wash car", id: 21 }
+      ],
+      id: 4
+    },
+    {
+      title: "logs",
+      data: [
+        { card: "mob rooms", id: 22 },
+        { card: "close window", id: 23 },
+        { card: "run errands", id: 24 },
+        { card: "water plants", id: 25 }
+      ],
+      id: 5
+    }
+  ]);
   return (
     <div className="task-board">
       <Navbar />
-      <CardColumn />
+      <CardColumn board={board} />
     </div>
   );
 }
