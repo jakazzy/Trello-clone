6d05038b0374009cda03cbbaa00bf48cf6e1f821
jakazzy <jakazzy@gmail.com>
2019-12-12 20:36:17 +0000

    fix linting

--- src/App.js
+++ src/App.js
@@ -1,80 +1,80 @@
-import React, { useState } from "react";
-import "./App.css";
-import CardColumn from "./containers/CardColumn/CardColumn";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
-import Navbar from "./components/navbar/Navbar";
-import data from "./data";
-import uuidv4 from "uuid/v4";
+import React, { useState } from 'react'
+import './App.css'
+import CardColumn from './containers/CardColumn/CardColumn'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+import Navbar from './components/navbar/Navbar'
+import data from './data'
+import uuidv4 from 'uuid/v4'
 
 function App() {
-  const [columns, setColumns] = useState(data);
-  const [addColumn, setAddColumn] = useState(false);
-  const [columnName, setColumnName] = useState({ column: "" });
+  const [columns, setColumns] = useState(data)
+  const [addColumn, setAddColumn] = useState(false)
+  const [columnName, setColumnName] = useState({ column: '' })
 
   const editCard = card => {
-    delete card.button;
-    delete card.focus;
-    const { id } = card;
+    delete card.button
+    delete card.focus
+    const { id } = card
     const newColumns = {
       ...columns,
       tasks: { ...columns.tasks, [id]: card }
-    };
-    setColumns({ ...newColumns });
-  };
+    }
+    setColumns({ ...newColumns })
+  }
 
   const createCard = (name, card, id) => {
-    const newColumns = columns;
-    newColumns.tasks[name] = card;
-    newColumns.columnsData[id].taskIds.push(name);
-    setColumns({ ...newColumns });
-  };
+    const newColumns = columns
+    newColumns.tasks[name] = card
+    newColumns.columnsData[id].taskIds.push(name)
+    setColumns({ ...newColumns })
+  }
 
   const removeCard = (card, columnId) => {
-    const { id } = card;
-    const data = columns;
+    const { id } = card
+    const data = columns
     data.columnsData[columnId].taskIds = data.columnsData[
       columnId
-    ].taskIds.filter(cardId => id !== cardId);
-    setColumns({ ...data });
-  };
+    ].taskIds.filter(cardId => id !== cardId)
+    setColumns({ ...data })
+  }
 
   const onDragEnd = results => {
-    const { destination, source, draggableId, type } = results;
+    const { destination, source, draggableId, type } = results
     if (!destination) {
-      return;
+      return
     }
     if (
       destination.droppableId === source.droppableId &&
       destination.index === source.index
     ) {
-      return;
+      return
     }
 
-    if (type === "column") {
-      const newColumnOrder = Array.from(columns.columnOrder);
-      newColumnOrder.splice(source.index, 1);
-      newColumnOrder.splice(destination.index, 0, draggableId);
+    if (type === 'column') {
+      const newColumnOrder = Array.from(columns.columnOrder)
+      newColumnOrder.splice(source.index, 1)
+      newColumnOrder.splice(destination.index, 0, draggableId)
 
       const newState = {
         ...columns,
         columnOrder: newColumnOrder
-      };
-      setColumns(newState);
-      return;
+      }
+      setColumns(newState)
+      return
     }
 
-    const start = columns.columnsData[source.droppableId];
-    const finish = columns.columnsData[destination.droppableId];
+    const start = columns.columnsData[source.droppableId]
+    const finish = columns.columnsData[destination.droppableId]
 
     if (start === finish) {
-      const newTaskIds = Array.from(start.taskIds);
+      const newTaskIds = Array.from(start.taskIds)
 
-      newTaskIds.splice(source.index, 1);
-      newTaskIds.splice(destination.index, 0, draggableId);
+      newTaskIds.splice(source.index, 1)
+      newTaskIds.splice(destination.index, 0, draggableId)
       const newColumn = {
         ...start,
         taskIds: newTaskIds
-      };
+      }
       const newData = {
         ...columns,
 
@@ -82,25 +82,25 @@ function App() {
           ...columns.columnsData,
           [newColumn.id]: newColumn
         }
-      };
-      setColumns(newData);
-      return;
+      }
+      setColumns(newData)
+      return
     }
 
-    const startTaskIds = Array.from(start.taskIds);
-    startTaskIds.splice(source.index, 1);
+    const startTaskIds = Array.from(start.taskIds)
+    startTaskIds.splice(source.index, 1)
     const newStart = {
       ...start,
       taskIds: startTaskIds
-    };
+    }
 
-    const finishTaskIds = Array.from(finish.taskIds);
-    finishTaskIds.splice(destination.index, 0, draggableId);
+    const finishTaskIds = Array.from(finish.taskIds)
+    finishTaskIds.splice(destination.index, 0, draggableId)
 
     const newFinish = {
       ...finish,
       taskIds: finishTaskIds
-    };
+    }
 
     const newState = {
       ...columns,
@@ -109,53 +109,53 @@ function App() {
         [newStart.id]: newStart,
         [newFinish.id]: newFinish
       }
-    };
-    setColumns(newState);
-  };
+    }
+    setColumns(newState)
+  }
 
   const moveCard = (card, column, previousposition, previousColumn, val) => {
-    val.pos = parseInt(val.pos - 1);
-    if (previousposition === val.pos && previousColumn === val.col) return;
+    val.pos = parseInt(val.pos - 1)
+    if (previousposition === val.pos && previousColumn === val.col) return
     let finishid = columns.columnOrder.filter(
       column => columns.columnsData[column].title === val.col
-    );
-    finishid = finishid.toString();
-    const start = columns.columnsData[column.id];
-    const finish = columns.columnsData[finishid];
+    )
+    finishid = finishid.toString()
+    const start = columns.columnsData[column.id]
+    const finish = columns.columnsData[finishid]
 
     if (previousColumn === val.col && previousposition !== val.pos) {
-      const newTaskIds = Array.from(start.taskIds);
-      const index = newTaskIds.indexOf(card.id);
-      newTaskIds.splice(index, 1);
-      newTaskIds.splice(val.pos, 0, card.id);
+      const newTaskIds = Array.from(start.taskIds)
+      const index = newTaskIds.indexOf(card.id)
+      newTaskIds.splice(index, 1)
+      newTaskIds.splice(val.pos, 0, card.id)
       const newColumn = {
         ...start,
         taskIds: newTaskIds
-      };
+      }
       const newData = {
         ...columns,
         columnsData: {
           ...columns.columnsData,
           [newColumn.id]: newColumn
         }
-      };
-      setColumns(newData);
-      return;
+      }
+      setColumns(newData)
+      return
     }
     if (previousColumn !== val.col && previousposition !== val.pos) {
-      const startTaskIds = Array.from(start.taskIds);
-      const index = startTaskIds.indexOf(card.id);
-      startTaskIds.splice(index, 1);
+      const startTaskIds = Array.from(start.taskIds)
+      const index = startTaskIds.indexOf(card.id)
+      startTaskIds.splice(index, 1)
       const newStart = {
         ...start,
         taskIds: startTaskIds
-      };
-      const finishTaskIds = Array.from(finish.taskIds);
-      finishTaskIds.splice(val.pos, 0, card.id);
+      }
+      const finishTaskIds = Array.from(finish.taskIds)
+      finishTaskIds.splice(val.pos, 0, card.id)
       const newFinish = {
         ...finish,
         taskIds: finishTaskIds
-      };
+      }
       const newState = {
         ...columns,
         columnsData: {
@@ -163,30 +163,30 @@ function App() {
           [newStart.id]: newStart,
           [newFinish.id]: newFinish
         }
-      };
-      setColumns(newState);
+      }
+      setColumns(newState)
     }
-  };
+  }
 
   const addList = () => {
-    setAddColumn(!addColumn);
-  };
+    setAddColumn(!addColumn)
+  }
 
   const addColumnName = event => {
-    setColumnName({ ...columnName, column: event.target.value });
-  };
+    setColumnName({ ...columnName, column: event.target.value })
+  }
 
   const addColumnDetails = event => {
-    event.preventDefault();
-    const columnid = uuidv4();
+    event.preventDefault()
+    const columnid = uuidv4()
     const column = {
       id: `column${columnid}`,
       title: columnName.column,
       taskIds: []
-    };
-    setColumnName({ column: "" });
+    }
+    setColumnName({ column: '' })
 
-    const columnList = columns.columnOrder;
+    const columnList = columns.columnOrder
 
     const newData = {
       ...columns,
@@ -195,24 +195,24 @@ function App() {
         [column.id]: column
       },
       columnOrder: [...columnList, column.id]
-    };
-    setAddColumn(!addColumn);
+    }
+    setAddColumn(!addColumn)
 
-    setColumns(newData);
-  };
+    setColumns(newData)
+  }
 
   const deleteColumn = columnid => {
     const newColumns = columns.columnOrder.filter(
       columnId => columnid !== columnId
-    );
+    )
 
     const newData = {
       ...columns,
       columnOrder: [...newColumns]
-    };
+    }
 
-    setColumns(newData);
-  };
+    setColumns(newData)
+  }
 
   const editColumnTitle = column => {
     const newData = {
@@ -221,9 +221,9 @@ function App() {
         ...columns.columnsData,
         [column.id]: column
       }
-    };
-    setColumns(newData);
-  };
+    }
+    setColumns(newData)
+  }
   return (
     <div className="task-board">
       <Navbar />
@@ -232,19 +232,17 @@ function App() {
           <Droppable
             droppableId="all-columns"
             direction="horizontal"
-            type="column"
-          >
+            type="column">
             {provided => (
               <div
                 className="sub-board"
                 {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
+                ref={provided.innerRef}>
                 {columns.columnOrder.map((columnValue, index) => {
-                  const column = columns.columnsData[columnValue];
+                  const column = columns.columnsData[columnValue]
                   const tasks = column.taskIds.map(
                     taskId => columns.tasks[taskId]
-                  );
+                  )
                   return (
                     <CardColumn
                       column={column}
@@ -259,7 +257,7 @@ function App() {
                       deleteColumn={deleteColumn}
                       editColumnTitle={editColumnTitle}
                     />
-                  );
+                  )
                 })}
                 {provided.placeholder}
               </div>
@@ -270,15 +268,13 @@ function App() {
           <div
             className="add-list add-board"
             onClick={addList}
-            style={{ display: !addColumn ? "inline-block" : "none" }}
-          >
+            style={{ display: !addColumn ? 'inline-block' : 'none' }}>
             <span className="plus-icon">+</span>
             <span>Add Another list</span>
           </div>
           <div
             className="textArea-add-list"
-            style={{ display: addColumn ? "inline-block" : "none" }}
-          >
+            style={{ display: addColumn ? 'inline-block' : 'none' }}>
             <form onSubmit={event => addColumnDetails(event)}>
               <input
                 id="add-list-textarea"
@@ -290,15 +286,14 @@ function App() {
                 dir="auto"
                 value={columnName.column}
                 maxLength="512"
-                onChange={event => addColumnName(event)}
-              ></input>
+                onChange={event => addColumnName(event)}></input>
               <button type="submit"> Add list</button>
             </form>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
-
-export default App;
+// linting src: https://thomlom.dev/setup-eslint-prettier-react/
+export default App
